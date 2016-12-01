dfbcb3f1947ac9fb418482b87d18c8b2d6742b76
Apoorva Balasaheb Jagtap <ajagtap2@uncc.edu>
2016-12-01 15:41:57 -0500

    Update ContentBasedRecommendation.py

--- ContentBased/ContentBasedRecommendation.py
+++ ContentBased/ContentBasedRecommendation.py
@@ -5,6 +5,7 @@ import numpy as np
 import random
 import csv
 import pdb
+from math import log10
 from pyspark import SparkConf,SparkContext
 
 def parseVector(line):
@@ -17,15 +18,14 @@ def parseVector(line):
 
 def calculateScore(line):
 	score = 0.0
-	artist = line.split('\t')[2]
-	genre = line.split('\t')[1]
-	#genreAvgRating = genreArtistRatingRows.map(lambda x: list(x[1])).map(lambda x: (x[0], float(x[2].encode('ascii', 'ignore')))).groupByKey().map(lambda x : (x[0], list(x[1]))).filter(lambda x: x[0]==str(genre)).mapValues(sum).map(lambda x: float(x[1])).collect()[0]
-	if(genre in broadCastedData):
-		score += broadCastedData.value[str(genre)]
-
-	#artistAvgRating = genreArtistRatingRows.map(lambda x: list(x[1])).map(lambda x: (x[1], float(x[2].encode('ascii', 'ignore')))).groupByKey().map(lambda x : (x[0], list(x[1]))).filter(lambda x: x[0]==str(artist)).mapValues(sum).map(lambda x: float(x[1])).collect()[0]
-	#score = float(genreAvgRating + artistAvgRating)
-	return line.split('\t')[0], score
+	line = line.split('\t')
+	artist = line[2]
+	genre = line[1]
+	if(genre in broadCastedData.value):
+		score += 1.0 + log10(broadCastedData.value[str(genre)])
+	if(artist in broadCastedData.value):
+		score += 1.0 + log10(broadCastedData.value[str(artist)])
+	return line[0], score
 	
 conf = SparkConf()
 sc = SparkContext(conf = conf)
@@ -34,16 +34,19 @@ data = sc.textFile("data1.txt")
 songsAttributes = sc.textFile("songatt.txt")
 userId = "c1"
 
-
 # [(i1,r1), (i2, r2)...] for logged in user
 itemRatings = data.map(parseVector).filter(lambda x: x[0] == userId).map(lambda x : (x[1][0],x[1][1]))
 itemRatingsMap = itemRatings.collectAsMap()
 
-genreArtistScores = songsAttributes.map(parseVector).filter(lambda x: x[0] in itemRatingsMap).flatMap(lambda x: x[1]).countByValue()
+#genreArtistScores = songsAttributes.map(parseVector).filter(lambda x: x[0] in itemRatingsMap).flatMap(lambda x: x[1]).countByValue()
 
 genreArtistRatingRows = songsAttributes.map(lambda x:x.encode("ascii","ignore")).map(parseVector).filter(lambda x: x[0] in itemRatingsMap).join(itemRatings).flatMapValues(lambda x:x).flatMapValues(lambda x:x).groupByKey()
 
-avgGenreRating = genreArtistRatingRows.map(lambda x: list(x[1])).map(lambda x: (x[0], float(x[2].encode('ascii', 'ignore')))).groupByKey().map(lambda x : (x[0], list(x[1]))).mapValues(sum).collectAsMap()
+avgGenreRating = genreArtistRatingRows.map(lambda x: list(x[1])).map(lambda x: (x[0], float(x[2].encode('ascii', 'ignore')))).groupByKey().map(lambda x : (x[0], list(x[1]))).mapValues(sum).map(lambda x: (x[1], x[0]))
 
-broadCastedData = sc.broadcast(avgGenreRating)
-scores = songsAttributes.map(calculateScore)
+avgArtistRating = genreArtistRatingRows.map(lambda x: list(x[1])).map(lambda x: (x[1], float(x[2].encode('ascii', 'ignore')))).groupByKey().map(lambda x : (x[0], list(x[1]))).mapValues(sum).map(lambda x: (x[1], x[0]))
+
+avgRating = avgGenreRating.union(avgArtistRating).map(lambda x: (x[1], x[0])).collectAsMap()
+
+broadCastedData = sc.broadcast(avgRating)
+recommendationList = songsAttributes.map(calculateScore).takeOrdered(50,key = lambda x: -x[1])
